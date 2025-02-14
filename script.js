8e280e3b6f4f51dc0badaf9ea68cc6211e4488e7
marius-bul <mariusbuljo@gmail.com>
2025-02-14 13:51:14 +0000

    fikser kjøpe funksjon

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function validateForm() {
     }
     return true; // Fortsetter innsendingen hvis passordene matcher
 }
+
+function buyProduct(productName) {
+    let username = prompt("Skriv inn brukernavnet ditt for å kjøpe:"); // Be brukeren skrive inn sitt navn
+
+    if (username) {
+        fetch("buy.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: `username=${encodeURIComponent(username)}&product=${encodeURIComponent(productName)}`
+        })
+        .then(response => response.text())
+        .then(data => alert(data))
+        .catch(error => console.error("Feil:", error));
+    }
+}
