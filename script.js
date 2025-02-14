c383a58ec352b35c1662e81fef01e1cda23c983b
marius-bul <mariusbuljo@gmail.com>
2025-02-14 12:25:26 +0000

    Henter verdi av passord, sjekker om passord er like, gir advarsel hvis det ikke er likt. Hvis passord matcher forsetter innsendingrn

---
+++ script.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    // Henter verdien fra passordfeltet
+    let pass = document.getElementById("password").value;
+    let confirmPass = document.getElementById("confirm_password").value;
+
+    // Sjekker om passordene er like
+    if (pass !== confirmPass) {
+        alert("Passordene matcher ikke!"); // Viser en advarsel hvis de ikke matcher
+        return false; // Stopper innsending av skjemaet
+    }
+    return true; // Fortsetter innsendingen hvis passordene matcher
+}
+
+
